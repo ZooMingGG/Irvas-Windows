ae448e2c0fce429493a012f995b55f5ffbff4566
ZooMingGG <volodia.lukashewwich@gmail.com>
2020-06-27 22:22:59 +0300

    did glazing slider

---
+++ src/js/slider.js
@@ -0,0 +1,32 @@
+$( document ).ready(function() {
+    $('.glazing-slider').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        prevArrow: $('.prev-arrow'),
+        nextArrow: $('.next-arrow'),
+        responsive: [
+            {
+              breakpoint: 900,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+        ]
+    });
+});
