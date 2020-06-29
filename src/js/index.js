d83d9eebae50963b143b58a8455d32e4acc6d9b3
ZooMingGG <volodia.lukashewwich@gmail.com>
2020-06-29 16:17:29 +0300

    remove some media for footer and add new feature for scroll btn, if user scroll down right now, scroll btn is hiden

--- src/js/index.js
+++ src/js/index.js
@@ -2,17 +2,27 @@
 
 function showScrollBtn() {
     const scrollBtn = document.querySelector('.scroll-btn');
+    let currentCords = 0;
 
     window.addEventListener('scroll', function(event) {
         if (window.pageYOffset > document.documentElement.clientHeight) {
-            scrollBtn.classList.add('visible');
+
+            if (document.documentElement.scrollTop < currentCords) {
+                scrollBtn.classList.add('visible');
+            } else {
+                scrollBtn.classList.remove('visible');
+            }
+
+            currentCords = document.documentElement.scrollTop;
         } else {
+            currentCords = 0;
             scrollBtn.classList.remove('visible');
         }
     });
 
     scrollBtn.addEventListener('click', function(event) {
         document.documentElement.scrollTo(0, 0);
+        currentCords = 0;
     });
 }
 
