6fedb2fdeb5a6dc7f4eeeb1114febe48be5460f9
ZooMingGG <volodia.lukashewwich@gmail.com>
2020-06-30 11:16:28 +0300

    did show modal functions

--- src/js/index.js
+++ src/js/index.js
@@ -1,29 +1,104 @@
 'use strict';
-
-function showScrollBtn() {
+document.addEventListener('DOMContentLoaded', function() {
     const scrollBtn = document.querySelector('.scroll-btn');
-    let currentCords = 0;
+    const overlay = document.querySelector('.overlay');
+    const headerPopupEngineerBtn = document.querySelector('.header-popup-btn');
+    const callLink = document.querySelector('.call-link');
+    const questionsLink = document.querySelector('.questions-link');
+    const showCalcModalButton = document.querySelectorAll('.calc-price-button');
+    const popupEngineer = document.querySelector('#popup-engineer');
+    const popupCall = document.querySelector('#popup-call');
+    const popupCalc = document.querySelector('#popup-calc');
+    const closeEngineerModalBtn = document.querySelector('.popup-engineer-close');
+    const closeCallModalBtn = document.querySelector('.popup-call-close');
 
-    window.addEventListener('scroll', function(event) {
-        if (window.pageYOffset > document.documentElement.clientHeight) {
+/*Function for showing and hiding modal windows*/
+    function showModals(trigger, modal, closeBtn) {
+        function showModal() {
+            overlay.classList.add('overlay-visible');
+            modal.classList.add('modal-visible');
+            document.body.style.overflow = 'hidden';
 
-            if (document.documentElement.scrollTop < currentCords) {
-                scrollBtn.classList.add('visible');
+            modal.addEventListener('click', function(event) {
+                let form = event.target.closest('form');
+
+                if (!form) {
+                    overlay.classList.remove('overlay-visible');
+                    modal.classList.remove('modal-visible');
+                    document.body.style.overflow = '';
+                }
+            });
+            
+            closeBtn.addEventListener('click', function() {
+                overlay.classList.remove('overlay-visible');
+                modal.classList.remove('modal-visible');
+                document.body.style.overflow = '';
+            });
+
+            clearTimeout(timerId);
+        } 
+
+        if (trigger === showCalcModalButton) {
+            trigger.forEach( function(item) {
+                item.addEventListener('click', showModal);
+            });
+        } else {
+            trigger.addEventListener('click', showModal);
+        }
+    }
+
+/*Function for showing popup call when user spend 60 seconds in site*/
+    let timerId = setTimeout( () => {
+        popupCall.classList.add('modal-visible');
+        overlay.classList.add('overlay-visible');
+        document.body.style.overflow = 'hidden';
+
+        popupCall.addEventListener('click', function(event) {
+            let form = event.target.closest('form');
+
+            if (!form) {
+                overlay.classList.remove('overlay-visible');
+                popupCall.classList.remove('modal-visible');
+                document.body.style.overflow = '';
+            }
+        });
+        
+        closeCallModalBtn.addEventListener('click', function(event) {
+            overlay.classList.remove('overlay-visible');
+            popupCall.classList.remove('modal-visible');
+            document.body.style.overflow = '';
+        });
+    }, 60000);
+
+    showModals(headerPopupEngineerBtn, popupEngineer, closeEngineerModalBtn);
+    showModals(callLink, popupCall, closeCallModalBtn);
+    showModals(questionsLink, popupCall, closeCallModalBtn);
+    showModals(showCalcModalButton, popupCalc, closeCallModalBtn);
+
+/*Function for show and hide scroll button*/
+    function showScrollBtn() {
+        let currentCords = 0;
+
+        window.addEventListener('scroll', function(event) {
+            if (window.pageYOffset > document.documentElement.clientHeight) {
+                if (document.documentElement.scrollTop < currentCords) {
+                    scrollBtn.classList.add('visible');
+                } else {
+                    scrollBtn.classList.remove('visible');
+                }
+
+                currentCords = document.documentElement.scrollTop;
             } else {
+                currentCords = 0;
                 scrollBtn.classList.remove('visible');
             }
+        });
 
-            currentCords = document.documentElement.scrollTop;
-        } else {
+        scrollBtn.addEventListener('click', function(event) {
+            document.documentElement.scrollTo(0, 0);
             currentCords = 0;
-            scrollBtn.classList.remove('visible');
-        }
-    });
+        });
+    }
 
-    scrollBtn.addEventListener('click', function(event) {
-        document.documentElement.scrollTo(0, 0);
-        currentCords = 0;
-    });
-}
-
-showScrollBtn();
+    showScrollBtn();
+});
