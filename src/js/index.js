7641ca1ce944955fbbf2d02c2ae6d144cc8b8464
ZooMingGG <volodia.lukashewwich@gmail.com>
2020-07-01 09:39:19 +0300

    add firebase to project

--- src/js/index.js
+++ src/js/index.js
@@ -28,6 +28,23 @@ document.addEventListener('DOMContentLoaded', function() {
     const glazingTabsContent = document.querySelectorAll('.glazing-items');
     const decorationTabsContent = document.querySelectorAll('.decoration-item');
     const modalCalcTabsContent = document.querySelectorAll('.big-img-icon');
+    const sendButton = document.querySelector('.send-btn');
+
+    // firebase.database().ref().child('contacts').push(variable);
+
+/*Initialize Firebase*/
+    const firebaseConfig = {
+        apiKey: "AIzaSyD1as3gwrYmKuFoBxQ1nvWIkRPnq8mV3iE",
+        authDomain: "irvas-windows.firebaseapp.com",
+        databaseURL: "https://irvas-windows.firebaseio.com",
+        projectId: "irvas-windows",
+        storageBucket: "irvas-windows.appspot.com",
+        messagingSenderId: "794720514305",
+        appId: "1:794720514305:web:f49e4543faf67b5961ab00",
+        measurementId: "G-Y64KPCRM58"
+    };
+
+    firebase.initializeApp(firebaseConfig);
 
 /*Function for showing and hiding modal windows*/
     function showModals(trigger, modal, closeBtn) {
